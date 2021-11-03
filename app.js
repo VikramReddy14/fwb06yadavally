86be6897626e708cc0d91f7ffae63fa2ac9d25ef
VikramReddy14 <89558329+VikramReddy14@users.noreply.github.com>
2021-11-03 15:40:15 -0500

    added addmods page

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var motelsRouter = require('./routes/motels');
+var addmodsRouter = require('./routes/addmods');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/motels', motelsRouter);
+app.use('/addmods', addmodsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
